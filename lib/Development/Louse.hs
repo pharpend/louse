b09a0364e324461b7311da3b1631785e02926d54
Peter Harpending <peter@harpending.org>
2015-06-03 21:41:11 -0600

    Annotate functions with Since: 0.1.0.0

--- lib/Development/Louse.hs
+++ lib/Development/Louse.hs
@@ -31,6 +31,8 @@
 -- This is the top-level module for the louse library. You only need to
 -- import this module, everything else will automatically be
 -- re-exported.
+-- 
+-- Since: 0.1.0.0
 
 module Development.Louse
   (-- *** Convenience re-exports
@@ -88,6 +90,8 @@ import Data.Time
 import Data.Tree
 
 -- |The type for a bug
+-- 
+-- Since: 0.1.0.0
 data Bug =
   Bug {bugTitle :: Title
       ,bugDescription :: Description
@@ -97,6 +101,8 @@ data Bug =
   deriving (Eq,Show)
 
 -- |'Bug' is trivially an instance of 'FromBug'
+-- 
+-- Since: 0.1.0.0
 instance FromBug Bug where
   fromBug = id
 
@@ -123,18 +129,25 @@ instance ToBug Bug where
 -- *** Exception: Title mustn't be empty.
 -- >>> fromString (mconcat (replicate 50 "foo")) :: Title
 -- *** Exception: Title mustn't be >64 characters long.
+-- 
+-- Since: 0.1.0.0
 newtype Title =
   Title {unTitle :: Text}
   deriving (Eq)
 
 -- |Compares by the value of @unTitle@.
+-- 
+-- Since: 0.1.0.0
 instance Ord Title where
   compare = comparing unTitle
 
+-- |Since: 0.1.0.0
 instance Show Title where
   show = T.unpack . unTitle
 
 -- |Note that this will throw an error if you give it an invalid value.
+-- 
+-- Since: 0.1.0.0
 instance IsString Title where
   fromString s =
     case mkTitle (T.pack s) of
@@ -143,6 +156,8 @@ instance IsString Title where
 
 -- |Attempt to make a title, returning an error message if the length is
 -- longer than 64 characters, or if the title is empty.
+-- 
+-- Since: 0.1.0.0
 mkTitle :: Text -> Exceptional Title
 mkTitle t
   | T.null t = fail "Title mustn't be empty."
@@ -166,18 +181,25 @@ mkTitle t
 -- 
 -- >>> "" :: Description
 -- *** Exception: Description mustn't be empty.
+-- 
+-- Since: 0.1.0.0
 newtype Description =
   Description {unDescription :: Text}
   deriving (Eq)
 
 -- |Compares by the value of 'unDescription'.
+-- 
+-- Since: 0.1.0.0
 instance Ord Description where
   compare = comparing unDescription
 
+-- |Since: 0.1.0.0
 instance Show Description where
   show = T.unpack . unDescription
 
 -- |Note that this will throw an error if given invalid input.
+-- 
+-- Since: 0.1.0.0
 instance IsString Description where
   fromString s =
     case mkDescription (T.pack s) of
@@ -186,12 +208,16 @@ instance IsString Description where
 
 -- |Attempt to make a description from a pure 'Text' value. This returns
 -- an error if the description is empty.
+-- 
+-- Since: 0.1.0.0
 mkDescription :: Text -> Exceptional Description
 mkDescription t
   | T.null t = fail "Description mustn't be empty."
   | otherwise = return (Description t)
 
 -- |Type for a person. Just has email and name
+-- 
+-- Since: 0.1.0.0
 data Person =
   Person {personName :: Text
          ,personEmail :: Text}
@@ -201,30 +227,46 @@ data Person =
 -- >>> Person "Joe Q. Public" "jqp@foo.bar.baz"
 -- Joe Q. Public <jqp@foo.bar.baz>
 -- it :: Person
+-- 
+-- Since: 0.1.0.0
 instance Show Person where
   show (Person n e) = T.unpack (mconcat [n," <",e,">"])
 
 -- |Alias for 'Person'
+-- 
+-- Since: 0.1.0.0
 type Author = Person
+
 -- |Alias for 'Person'
+-- 
+-- Since: 0.1.0.0
 type Reporter = Person
 
 -- |The type for a comment
+-- 
+-- Since: 0.1.0.0
 data Comment =
-  Comment {commentAuthor :: Author
-          ,commentText :: CommentText
-          ,subComments :: CommentTree}
+  Comment 
+  {commentAuthor :: Author
+  ,commentText :: CommentText
+  ,subComments :: CommentTree}
   deriving (Eq,Show)
 
 -- |Comment text has the same requirements as a 'Description', so alias
--- the two:
+-- the two
+-- 
+-- Since: 0.1.0.0
 type CommentText = Description
    
 -- |Alias for 'mkDescription'
+-- 
+-- Since: 0.1.0.0
 mkCommentText :: Text -> Exceptional CommentText
 mkCommentText = mkDescription
 
 -- |Alias for 'unDescription'
+-- 
+-- Since: 0.1.0.0
 unCommentText :: CommentText -> Text
 unCommentText = unDescription
 
@@ -232,10 +274,14 @@ unCommentText = unDescription
 -- using lazy 'HashMap's.
 -- 
 -- Specifically, this is a newtype over 'HashMap' 'ByteString' 'Comment'. The idea being that the key 
+-- 
+-- Since: 0.1.0.0
 newtype CommentTree =
-  CommentTree {unCommentTree :: HashMap ByteString Comment}
-  deriving (Eq, Show)
+  CommentTree   -- |Since: 0.1.0.0
+   {unCommentTree :: HashMap ByteString Comment}
+  deriving (Eq,Show)
   
+-- |Since: 0.1.0.0
 instance ToForest CommentTree (Author,CommentText) where
   toForest commentTree =
     do (_,Comment auth txt subcomments) <-
@@ -248,27 +294,39 @@ class ToBug a  where
   toBug :: a -> Bug
   
 -- |Convert something from a 'Bug'
+-- 
+-- Since: 0.1.0.0
 class FromBug a where
   fromBug :: Bug -> a
 
 -- |Convert something of type @foo@ to a 'Tree' of type @bar@.
+-- 
+-- Since: 0.1.0.0
 class ToTree foo bar where
   toTree :: foo -> Tree bar
 
 -- |Convert a 'Tree' of type @bar@s to something of type @foo@.
+-- 
+-- Since: 0.1.0.0
 class FromTree bar foo where
   fromTree :: Tree bar -> foo
 
 -- |Convert something of type @foo@ to a 'Forest' of @bar@s.
+-- 
+-- Since: 0.1.0.0
 class ToForest foo bar where
   toForest :: foo -> Forest bar
 
+-- |Since: 0.1.0.0
 instance (ToTree foo bar,Foldable t) => ToForest (t foo) bar where
   toForest = foldMap (\baz -> [toTree baz])
 
 -- |Convert a 'Forest' of type @bar@ to something of type @foo@.
+-- 
+-- Since: 0.1.0.0
 class FromForest bar foo where
   fromForest :: Forest bar -> foo
 
+-- |Since: 0.1.0.0
 instance (FromTree bar foo) => FromForest bar [foo] where
   fromForest = map fromTree
