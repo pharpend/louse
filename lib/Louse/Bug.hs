bcad963bb975c4ce67504fa71e8aa1b42211319a
Peter Harpending <peter@harpending.org>
2015-06-22 12:52:03 -0600

    Add Bug tagging.

--- lib/Louse/Bug.hs
+++ lib/Louse/Bug.hs
@@ -1,20 +1,20 @@
 -- louse - distributed bugtracker
 -- Copyright (c) 2015, Peter Harpending.
--- 
+--
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation, either version 3 of the License, or (at
 -- your option) any later version.
--- 
+--
 -- This program is distributed in the hope that it will be useful, but
 -- WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 -- General Public License for more details.
--- 
+--
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
--- | 
+-- |
 -- Module      : Louse.Bug
 -- Description : The type for a Louse Bug
 -- Copyright   : Copyright (c) 2015, Peter Harpending.
@@ -22,7 +22,7 @@
 -- Maintainer  : Peter Harpending <peter@harpending.org>
 -- Stability   : experimental
 -- Portability : UNIX/GHC
--- 
+--
 -- The type for a louse bug.
 
 module Louse.Bug where
@@ -41,7 +41,7 @@ import Data.Time
 import Data.Yaml
 
 -- |The type for a bug
--- 
+--
 -- Since: 0.1.0.0
 data Bug =
   Bug {bugTitle :: Title
@@ -49,17 +49,18 @@ data Bug =
       ,bugAuthor :: Author
       ,bugTime :: UTCTime
       ,bugComments :: [Comment]
+      ,bugTags :: [Tag]
       ,bugOpen :: Bool}
   deriving (Eq,Show)
 
 -- |'Bug' is trivially an instance of 'FromBug'
--- 
+--
 -- Since: 0.1.0.0
 instance FromBug Bug where
   fromBug = id
 
 -- |'Bug' is trivially an instance of 'ToBug'
--- 
+--
 -- Since: 0.1.0.0
 instance ToBug Bug where
   toBug = id
@@ -68,10 +69,11 @@ instance ToBug Bug where
 instance FromJSON Bug where
   parseJSON (Object v) = Bug <$> v .: "bug-title"
                              <*> v .: "bug-description"
-                             <*> (v .: "bug-author" <|> 
+                             <*> (v .: "bug-author" <|>
                                   v .: "bug-reporter")
                              <*> v .: "bug-time"
                              <*> v .: "bug-comments"
+                             <*> v .: "bug-tags"
                              <*> v .: "bug-open"
 
 -- | Since: 0.1.0.0
@@ -79,34 +81,77 @@ instance Ord Bug where
   compare = comparing bugTime
 
 -- |Typeclass to convert something to a 'Bug'
--- 
+--
 -- Since: 0.1.0.0
 class ToBug a  where
   toBug :: a -> Bug
 
 -- |Convert something from a 'Bug'
--- 
+--
 -- Since: 0.1.0.0
 class FromBug a where
   fromBug :: Bug -> a
 
+-- |Newtype over 'Text'. Allows nonempty 'Text' values, <= 32
+-- chars. Chars must all be elements of 'acceptChars'
+--
+-- Since: 0.1.0.0
+newtype Tag = Tag {unTag :: Text}
+  deriving (Eq, Show)
+
+-- |Attempt to make a 'Tag'
+--
+-- Since: 0.1.0.0
+mkTag :: Text -> Exceptional Tag
+mkTag txt
+  | T.null txt = fail "Tag must be non-empty"
+  | T.any (not . flip elem acceptChars) txt =
+      fail $ mappend "Tag may only contain the following characters: "
+                     acceptChars
+  | otherwise = return $ Tag txt
+
+-- |Acceptable characters in a 'Tag'
+acceptChars :: [Char]
+acceptChars = mconcat [ ['A'..'Z']
+                      , ['a'..'z']
+                      , ['0'..'9']
+                      , "-_./+"
+                      ]
+
+-- |Throws error on invalid data
+-- 
+-- Since: 0.1.0.0
+instance IsString Tag where
+  fromString s = case mkTag (T.pack s) of
+                   Failure s -> error s
+                   Success x -> x
+
+-- |Since: 0.1.0.0
+instance FromJSON Tag where
+  parseJSON (String s) = runExceptional $ mkTag s
+  parseJSON _ = mzero
+  
+-- |Since: 0.1.0.0
+instance ToJSON Tag where
+  toJSON (Tag t) = String t
+
 -- |Yet another newtype over 'Text'. This is to make sure the
 -- description is less than (or equal to) 8192 characters.
--- 
+--
 -- Use 'mkDescription' to make a description. This is an instance of
 -- 'IsString', too, so, in pure code, you can just write plain strings,
 -- and turn on the OverloadedStrings extension.
--- 
--- >>> :set -XOverloadedStrings 
+--
+-- >>> :set -XOverloadedStrings
 -- >>> "hello" :: Description
 -- Description {unDescription = "hello"}
 -- it :: Description
--- 
+--
 -- If you give invalid input, then there will be an error:
--- 
+--
 -- >>> "" :: Description
 -- *** Exception: Description mustn't be empty.
--- 
+--
 -- Since: 0.1.0.0
 newtype Description =
   Description {unDescription :: Text}
@@ -114,7 +159,7 @@ newtype Description =
 
 -- |Attempt to make a description from a pure 'Text' value. This returns
 -- an error if the description is empty.
--- 
+--
 -- Since: 0.1.0.0
 mkDescription :: Text -> Exceptional Description
 mkDescription t
@@ -122,7 +167,7 @@ mkDescription t
   | otherwise = return (Description t)
 
 -- |Compares by the value of 'unDescription'.
--- 
+--
 -- Since: 0.1.0.0
 instance Ord Description where
   compare = comparing unDescription
@@ -132,7 +177,7 @@ instance Show Description where
   show = T.unpack . unDescription
 
 -- |Note that this will throw an error if given invalid input.
--- 
+--
 -- Since: 0.1.0.0
 instance IsString Description where
   fromString s =
@@ -150,25 +195,25 @@ instance ToJSON Description where
   toJSON (Description s) = String s
 
 -- |A newtype over 'Text'. Haskell doesn't have dependent types, so I
--- have to use a hack called "smart constructors" to make sure 
--- 
+-- have to use a hack called "smart constructors" to make sure
+--
 -- > 0 < title_length <= 64
--- 
+--
 -- Use 'mkTitle' to make a title. Alternatively, you could turn on
 -- OverloadedStrings, and use 'Title''s 'IsString' instance:
--- 
--- >>> :set -XOverloadedStrings 
+--
+-- >>> :set -XOverloadedStrings
 -- >>> "hello" :: Title
 -- Title {unTitle = "hello"}
 -- it :: Title
--- 
+--
 -- Note that if you give invalid input, then there will be an error:
--- 
+--
 -- >>> "" :: Title
 -- *** Exception: Title mustn't be empty.
 -- >>> fromString (mconcat (replicate 50 "foo")) :: Title
 -- *** Exception: Title mustn't be >64 characters long.
--- 
+--
 -- Since: 0.1.0.0
 newtype Title =
   Title {unTitle :: Text}
@@ -176,7 +221,7 @@ newtype Title =
 
 -- |Attempt to make a title, returning an error message if the length is
 -- longer than 64 characters, or if the title is empty.
--- 
+--
 -- Since: 0.1.0.0
 mkTitle :: Text -> Exceptional Title
 mkTitle t
@@ -185,7 +230,7 @@ mkTitle t
   | otherwise = return (Title t)
 
 -- |Compares by the value of @unTitle@.
--- 
+--
 -- Since: 0.1.0.0
 instance Ord Title where
   compare = comparing unTitle
@@ -195,7 +240,7 @@ instance Show Title where
   show = T.unpack . unTitle
 
 -- |Note that this will throw an error if you give it an invalid value.
--- 
+--
 -- Since: 0.1.0.0
 instance IsString Title where
   fromString s =
@@ -213,7 +258,7 @@ instance ToJSON Title where
   toJSON (Title s) = String s
 
 -- |A nice little report explaining the status of a set of bugs.
--- 
+--
 -- Since: 0.1.0.0
 status :: FilePath              -- ^The directory holding the status report
        -> [Bug]                 -- ^The list of bugs
@@ -223,17 +268,17 @@ status fp bugs =
           , ""
           , "Number of open bugs: " ++ show (openBugs bugs)
           , "Number of closed bugs: " ++ show (closedBugs bugs)
-          , "Number of "
+          , "Number of closed "
           ]
 
 -- |Count the number of open bugs in a list.
--- 
+--
 -- Since: 0.1.0.0
 openBugs :: [Bug] -> Int
 openBugs = length . filter bugOpen
 
 -- |Count the number of closed bugs in a list.
--- 
+--
 -- Since: 0.1.0.0
 closedBugs :: [Bug] -> Int
 closedBugs = length . filter (not . bugOpen)
