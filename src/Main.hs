cc73008aebc3bda8514faf99a49af228ca0ad883
Peter Harpending <peter@harpending.org>
2016-03-01 00:29:45 -0700

    Add a new main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -26,5 +26,71 @@
 
 module Main where
 
+import Control.Applicative
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import Data.FileEmbed
+import qualified Data.Text.Encoding as TE
+import Options.Applicative
+import System.IO
+import System.Pager
+
+altConcat :: (Foldable t, Alternative f) => t (f a) -> f a
+altConcat = foldr (<|>) empty
+
+infoHelper x = info (helper <*> x)
+
 main :: IO ()
-main = putStrLn "Hello world"
+main = execParser mainPI >>= runArgs
+  where
+    mainPI =
+      infoHelper actionParser $
+        mconcat [ fullDesc
+                , progDesc "This is a small program to fetch bug information from remote trackers, and browse the information locally."
+                ]
+
+    actionParser = altConcat (fmap subparser [ licenseParser
+                                             , readmeParser
+                                             ])
+          
+data Args = License ShowOpts
+          | Readme ShowOpts
+          | FetchIssues String S
+  deriving (Show, Eq)     
+
+data ShowOpts = ShowOpts { soNoPager :: Bool
+                         }
+  deriving (Show, Eq)     
+
+soParser :: Parser ShowOpts
+soParser =
+  ShowOpts <$> switch (mconcat [ long "no-pager"
+                               , help "Disable automatic piping to the system pager (usually called 'less' or 'more')."
+                               ])
+
+readmeParser :: Mod CommandFields Args
+readmeParser =
+  command "readme" $
+    infoHelper (fmap Readme soParser) 
+               (mappend fullDesc
+                         (progDesc "Show the README."))
+  
+licenseParser :: Mod CommandFields Args
+licenseParser =
+  command "license" $
+    infoHelper (fmap License soParser)
+               (mappend fullDesc
+                         (progDesc "Show the license (GPL-3)."))
+
+
+runArgs :: Args -> IO ()
+runArgs args = do
+    hSetBuffering stdout NoBuffering
+    case args of
+      License so -> show' so $(embedFile "LICENSE")
+      Readme so -> show' so $(embedFile "README.md")
+
+show' :: ShowOpts -> ByteString -> IO ()
+show' so bs
+  | soNoPager so = B.hPut stdout bs
+  | otherwise = printOrPage (TE.decodeUtf8 bs)
